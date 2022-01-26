2d1c47a27439486c96f872998e1ca3467935254d
Eren <eren.sertkaya@kozmostekstil.com>
2022-01-26 17:38:52 +0300

    feat: Add sweetAjax.js

---
+++ sweetAjax.js
@@ -0,0 +1,143 @@
+$.widget( "sa.sweetAjax", {
+
+    // Default options.
+    options: {
+        autoAlert:false,
+        sweetAlert:null,
+        sweetAlertOptions:{},
+        redirect:false,
+        redirectURL: "/user/access/login",
+        defaultErrorFields:{
+            heading: "heading",
+            message: "message",
+            type: "type",
+        },
+        default500Error:{
+            heading: "Internal Server Error",
+            message: "Try again, if it  continues contact with IT team.",
+            type: "error",
+        }
+    },
+
+    _create: function() {
+        let data = {
+            title:this.options.data.heading,
+            text:this.options.data.message,
+            icon: this.options.data.type,
+        }
+
+        let ajaxOptions = {
+            ...data,
+            ...this.options,
+        }
+
+        //remove unnecessary things
+        for (option in ajaxOptions){
+            if (option === "classes" || option === "disabled" || option === "create"){
+                delete ajaxOptions[option];
+            }
+        }
+
+        // add complete handler if none of the ajax handlers not sent
+        if (!Object.keys(ajaxOptions).includes("complete") && !Object.keys(ajaxOptions).includes("success") && !Object.keys(ajaxOptions).includes("error")){
+            ajaxOptions.complete = function (){}
+        }
+
+        // add additional functionality to ajax handlers with proxy pattern
+        if (this.options.autoAlert === true){
+            for (option in ajaxOptions){
+                if (option === "complete"){
+                    let proxiedComplete = ajaxOptions[option]; // Preserving original function
+                    ajaxOptions[option] = (jqXHR, textStatus) => {
+                            if (jqXHR.responseJSON){
+                                this._alert({
+                                    title: jqXHR.responseJSON[this.options.defaultErrorFields.heading],
+                                    text: jqXHR.responseJSON[this.options.defaultErrorFields.message],
+                                    icon: jqXHR.responseJSON[this.options.defaultErrorFields.type],
+                                },jqXHR.status);
+                            }
+                            else{
+                                this._alert({
+                                    title: this.options.default500Error[this.options.defaultErrorFields.heading],
+                                    text: this.options.default500Error[this.options.defaultErrorFields.message],
+                                    icon: this.options.default500Error[this.options.defaultErrorFields.type],
+                                },jqXHR.status);
+                            }
+                        return proxiedComplete.apply(this, arguments);
+                    }
+
+                }
+                if (option === "success"){
+                    let proxiedSuccess = ajaxOptions[option]; // Preserving original function
+                    ajaxOptions[option] = (data,textStatus,jqXHR) => {
+                            this._alert({
+                                title: jqXHR.responseJSON[this.options.defaultErrorFields.heading],
+                                text: jqXHR.responseJSON[this.options.defaultErrorFields.message],
+                                icon: jqXHR.responseJSON[this.options.defaultErrorFields.type],
+                            },jqXHR.status);
+                        return proxiedSuccess.apply(this, arguments);
+                    }
+
+                }
+                if (option === "error"){
+                    let proxiedError = ajaxOptions[option]; // Preserving original function
+                    ajaxOptions[option] = (jqXHR,textStatus,errorThrown ) => {
+                            this._alert({
+                                title: this.options.default500Error[this.options.defaultErrorFields.heading],
+                                text: this.options.default500Error[this.options.defaultErrorFields.message],
+                                icon: this.options.default500Error[this.options.defaultErrorFields.type],
+                            },jqXHR.status);
+                        return proxiedError.apply(this, arguments);
+                    }
+
+                }
+            }
+        }
+        this._ajax(ajaxOptions);
+    },
+
+    _ajax: function (ajaxOptions){
+        let ajax = $.ajax({
+            ...ajaxOptions
+            }
+        )
+
+        //merge ajax prototype with plugin's prototype without cloning, for accessing ajax chain methods from plugin when it's initialized
+        this.__proto__ = this._inPlaceMerge(this.__proto__,ajax);
+    },
+
+    _alert: function (data, statusCode){
+        if (this.options.sweetAlert === true){
+            Swal.fire({
+                ...data,
+                ...this.options.sweetAlertOptions
+            }).then((result) => {
+                this._trigger("afterSweetAlert",null,result);
+                    this._redirect(statusCode);
+            })
+        }
+        if (this.options.sweetAlert === false ){
+            alert(data.text);
+            this._redirect(statusCode);
+        }
+    },
+
+    _redirect: function (statusCode){
+        if (statusCode === 401 && this.options.redirect === true){
+            window.location.href  = window.location.origin + this.options.redirectURL
+        }
+    },
+
+    _inPlaceMerge: function(tar, src) {
+        let res = tar;
+        if (typeof src === "object"  && typeof tar === "object") {
+            for (key in src){
+                res[key] = this._inPlaceMerge(tar[key], src[key]);
+            }
+        } else if (src !== undefined) {
+            res = src;
+        }
+        return res;
+    }
+
+});
